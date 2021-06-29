3062f796c7aba35cc83dcca3b9ae8cb76973012f
rkudlar <rkudla.dev@gmail.com>
2021-06-29 19:29:28 +0300

    annotate paper

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: papers
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Paper < ApplicationRecord
 end
