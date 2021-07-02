f5ed8e2ba885d6919e381096f7da9b7b78f7a747
rkudlar <rkudla.dev@gmail.com>
2021-07-02 17:22:17 +0300

    connect bootstrap and add styles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "bootstrap"
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-import "bootstrap"
-import "../stylesheets/application.scss"
-
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip()
-  $('[data-toggle="popover"]').popover()
-})
 
 Rails.start()
 Turbolinks.start()
