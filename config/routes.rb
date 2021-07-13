8497b4dbda3a7bc8f2469a69742bb058d011b14c
rkudlar <rkudla.dev@gmail.com>
2021-07-14 01:18:32 +0300

    fix sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@
 #
 
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
 root to: "home#index"
+
+devise_for :users
+
+devise_scope :user do
+   get '/users/sign_out' => 'devise/sessions#destroy'
+end
 
 resources :paper
 end
