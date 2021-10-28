c621539aa9bf5f496fb86fdfbdd1c839049aa208
rkudlar <rkudla.dev@gmail.com>
2021-10-28 15:54:28 +0300

    add model link and input field

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_122103) do
+ActiveRecord::Schema.define(version: 2021_10_28_120512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "links", force: :cascade do |t|
+    t.text "long_link"
+    t.string "short_link"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "papers", force: :cascade do |t|
     t.string "title"
